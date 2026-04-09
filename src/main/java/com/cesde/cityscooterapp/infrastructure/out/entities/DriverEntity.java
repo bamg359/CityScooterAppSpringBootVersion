6d5cae05c6bf638a3b777729eaec3871c56b5ebc
cuervok32 <cuervo95simon@gmail.com>
2026-04-08 21:11:38 -0500

    feat se crean las concecciones entre los servicios y las interfaces usando una nueva arquitectura

---
+++ src/main/java/com/cesde/cityscooterapp/infrastructure/out/entities/DriverEntity.java
@@ -0,0 +1,34 @@
+package com.cesde.cityscooterapp.infrastructure.out.entities;
+
+import com.cesde.cityscooterapp.domain.enums.IsPenalizedEnum;
+import com.cesde.cityscooterapp.domain.enums.TypeDriverEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "drivers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DriverEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "usar_name", nullable = false)
+    private String name;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    private String phone;
+    private String email;
+    private String password;
+    private boolean status;
+    @Column (name = "type_driver", nullable = false)
+    private TypeDriverEnum typeDriver;
+    private  Integer points;
+    private IsPenalizedEnum isPenalized;
+}
---
+++ src/main/java/com/cesde/cityscooterapp/infrastructure/out/repository/DriverRepository.java
@@ -0,0 +1,7 @@
+package com.cesde.cityscooterapp.infrastructure.out.repository;
+
+import com.cesde.cityscooterapp.infrastructure.out.entities.DriverEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverRepository extends JpaRepository<DriverEntity, Long> {
+}
